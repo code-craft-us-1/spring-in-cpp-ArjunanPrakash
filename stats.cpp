732d063211666451695f933fcba19e6ee86066a9
Prakash Arjunan <104984016+ArjunanPrakash@users.noreply.github.com>
2024-08-27 20:01:03 +0530

    Backward compatibility check C++14 versioned

--- stats.cpp
+++ stats.cpp
@@ -1,9 +1,8 @@
 #include "stats.h"
 #include <vector>
-#include <cmath>    // For NAN and std::abs
-#include <numeric>  // For std::reduce
-#include <ranges>   // For ranges and views
-#include <algorithm> // FOr max and min
+#include <cmath>  // For NAN and std::abs
+#include <numeric> // For std::accumulate
+#include <algorithm> //For std::max_element and std::min_element
 
 namespace Statistics
 {
@@ -19,13 +18,13 @@ namespace Statistics
             return result;
         }
 
-        // Calculate the average using std::reduce and ranges
+        // Calculate the average value of the elements in the data vector.
         auto const count = static_cast<float>(data.size());
-        result.average = std::reduce(data.begin(), data.end(), 0.0f) / count;
+        result.average = std::accumulate(data.begin(), data.end(), 0.0f) / count;
 
-        // Calculate max and min using std::ranges
-        result.max = std::ranges::max(data);
-        result.min = std::ranges::min(data);
+        // Calculate the maximum and minimum values in the data vector.
+        result.max = *std::max_element(data.begin(), data.end());
+        result.min = *std::min_element(data.begin(), data.end());
 
         return result;
     }
